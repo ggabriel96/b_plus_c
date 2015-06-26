0b286f52b4386bb44d4b7de8cf88c9031b7de97b
Gabriel Galli <g7.galli96@gmail.com>
2015-06-25 22:56:05 -0300

    Reading and writing a node is working

--- b_plus.c
+++ b_plus.c
@@ -21,22 +21,46 @@ void throw(char *message) {
     exit(1);
 }
 
-void write(node_t node) {
+void print(node_t *node) {
+    int i;
 
+    printf("count: %d\n", node -> count);
+    for (i = 0; i < B; i++) {
+        printf("key[%d]: %d\n", i, node -> key[i]);
+    }
+    for (i = 0; i < B + 1; i++) {
+        printf("index[%d]: %ld\n", i, node -> index[i]);
+    }
+}
+
+void write(node_t *node, long index) {
+    // moving to "index" on "treeFile" file
+    // from SEEK_SET (beginning of the file)
+    // if successful, fseek returns 0
+    if (fseek(treeFile, index, SEEK_SET)) throw("fseek failed @ write()");
+    if (fwrite(node, sizeof (node_t), 1, treeFile) == 0) throw("fwrite failed @ write()");
 }
 
 void read(node_t *dest, long index) {
     // moving to "index" on "treeFile" file
     // from SEEK_SET (beginning of the file)
     // if successful, fseek returns 0
-    if (fseek(treeFile, index, SEEK_SET)) throw("fseek failed, line 31");
+    if (fseek(treeFile, index, SEEK_SET)) throw("fseek failed @ read()");
     // reads 1 node_t from "treeFile" and stores it into "dest"
     // fread returns the number of elements read
-    if ((fread(dest, sizeof (node_t), 1, treeFile)) == 0) throw("fread failed, line 33");
+    if ((fread(dest, sizeof (node_t), 1, treeFile)) == 0) throw("fread failed @ read()");
 }
 
-void insert(node_t tree, int key) {
+void insert(node_t *tree, int key) {
+    node_t rd;
+    node_t *wr = malloc(sizeof (node_t));
+    wr -> count = 0;
+    wr -> key[0] = 7;
+    wr -> index[0] = NIL;
 
+    write(wr, 0);
+    read(&rd, 0);
+    print(&rd);
 }
 
 int main(int argc, char const *argv[]) {
@@ -47,7 +71,8 @@ int main(int argc, char const *argv[]) {
     treeFile = fopen(filename, "w+b");
     if (treeFile != NULL) {
         for (i = 0; i < INS_MAX; i++) {
-            insert(root, i);
+            printf("i: %d\n", i);
+            insert(&root, i);
         }
     }
     else {
