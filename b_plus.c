672183138dc813c0477bd6eed17c43ecdcb7bb87
Gabriel Galli <g7.galli96@gmail.com>
2015-07-02 21:42:23 -0300

    Simple ascending insertion working? LOL

--- b_plus.c
+++ b_plus.c
@@ -7,7 +7,7 @@
 #define NIL (-1L)
 // INFTY tells us that this is a "null node"
 #define INFTY 2e32
-#define INS_MAX 4
+#define INS_MAX 10
 
 #define INFO 0
 #define PTR 1
@@ -18,8 +18,7 @@ typedef struct BPNode {
 } node_t;
 
 FILE *treeFile;
-node_t *root;
-long diskIndex;
+long rootIndex, diskIndex;
 
 void write(node_t *node, long index);
 void read(node_t *dest, long index);
@@ -54,7 +53,10 @@ node_t * newNode() {
 void print(node_t *node) {
     int i;
 
-    if (node == NULL) return;
+    if (node == NULL) {
+        printf("# print(NULL) #");
+        return;
+    }
 
     printf("index: %ld\n", node -> index);
     printf("parent: %ld\n", node -> parent);
@@ -66,6 +68,7 @@ void print(node_t *node) {
 }
 
 void write(node_t *node, long index) {
+    if (node -> parent == NIL) rootIndex = node -> index;
     // moving to "index" on "treeFile" file
     // from SEEK_SET (beginning of the file)
     // if successful, fseek returns 0
@@ -107,185 +110,6 @@ void delete(node_t **tree, long key) {
     }
 
     // delFix();
-}
-
-node_t * find(node_t *node, long key) {
-    int i;
-    long toRead;
-
-    if (node != NULL) {
-        for (i = 0; i < B; i++) {
-            if (node -> key[i][INFO] > key) {
-                if (i > 0) {
-                    printf(">>>> 1\n");
-                    toRead = node -> key[i - 1][PTR];
-                    node = (node_t *) malloc(sizeof (node_t));
-                    read(node, toRead);
-                    i = -1;
-                }
-                else {
-                    if (node -> key[i][PTR] == NIL) {
-                        printf(">>>> 2\n");
-                        return node;
-                    }
-                    else {
-                        printf(">>>> 3\n");
-                        read(node, node -> key[i][PTR]);
-                        i = -1;
-                    }
-                }
-            }
-            else if (node -> key[i][INFO] == key) {
-                if (node -> key[i][PTR] == NIL) {
-                    return node;
-                }
-                else {
-                    toRead = node -> key[i][PTR];
-                    node = (node_t *) malloc(sizeof (node_t));
-                    read(node, toRead);
-                    i = -1;
-                }
-            }
-        }
-    }
-
-    return NULL;
-}
-
-void insert(node_t **tree, long key, long ptr) {
-    int i;
-    node_t *node = NULL;
-
-    if (*tree == NULL) (*tree) = newNode();
-    node = find(*tree, key);
-
-    print(node);
-
-    for (i = 0; i < B; i++) {
-        if (node -> key[i][INFO] == (long) INFTY) {
-            node -> key[i][INFO] = key;
-            node -> key[i][PTR] = ptr;
-            node -> count++;
-            // the matrix to sort, quantity of lines, size of each line, comparing function
-            qsort(node -> key, B, sizeof (long) * 2, &cmp);
-            break;
-        }
-    }
-
-    addFix(&node);
-    write(node, node -> index);
-}
-
-void addFix(node_t **node) {
-    int i, j, changedParent = 0;
-    long firstKey = NIL;
-    node_t *parent = NULL, *left = NULL, *right = NULL, *tmp = NULL;
-
-    if ((*node) -> parent != NIL) {
-        parent = (node_t *) malloc(sizeof (node_t));
-        read(parent, (*node) -> parent);
-
-        if (parent -> key[0][INFO] > (*node) -> key[0][INFO]) {
-            parent -> key[0][INFO] = (*node) -> key[0][INFO];
-            changedParent = 1;
-        }
-    }
-
-    if ((*node) -> count >= B) {
-        firstKey = (*node) -> key[0][INFO];
-
-        for (i = 0; i < B / 2; i++) {
-            insert(&left, (*node) -> key[i][INFO], (*node) -> key[i][PTR]);
-
-            // updating children's parent
-            if (left -> key[i][PTR] != NIL) {
-                tmp = (node_t *) malloc(sizeof (node_t));
-                read(tmp, left -> key[i][PTR]);
-
-                tmp -> parent = left -> index;
-                write(tmp, left -> key[i][PTR]);
-
-                free(tmp);
-                tmp = NULL;
-            }
-        }
-
-        for (j = 0; i < B; i++, j++) {
-            insert(&right, (*node) -> key[i][INFO], (*node) -> key[i][PTR]);
-
-            // updating children's parent
-            if (right -> key[i][PTR] != NIL) {
-                tmp = (node_t *) malloc(sizeof (node_t));
-                read(tmp, right -> key[i][PTR]);
-
-                tmp -> parent = right -> index;
-                write(tmp, right -> key[i][PTR]);
-
-                free(tmp);
-                tmp = NULL;
-            }
-        }
-
-        // it's the root of the tree
-        if ((*node) -> parent == NIL) {
-            free(*node);
-            *node = NULL;
-
-            // first keys of left and right
-            insert(node, left -> key[0][INFO], left -> index);
-            insert(node, right -> key[0][INFO], right -> index);
-
-            left -> parent = (*node) -> index;
-            right -> parent = (*node) -> index;
-
-            write((*node), (*node) -> index);
-        }
-        else {
-            delete(&parent, firstKey);
-            insert(&parent, left -> key[0][INFO], left -> index);
-            insert(&parent, right -> key[0][INFO], right -> index);
-
-            left -> parent = parent -> index;
-            right -> parent = parent -> index;
-
-            write(parent, parent -> index);
-        }
-
-        write(left, left -> index);
-        write(right, right -> index);
-    }
-
-    if (parent != NULL && changedParent) {
-        write(parent, parent -> index);
-        addFix(&parent);
-        free(parent);
-        parent = NULL;
-    }
-}
-
-int main(int argc, char const *argv[]) {
-    int i;
-    char filename[] = "bptree.bin";
-
-    diskIndex = 0;
-    root = newNode();
-    treeFile = fopen(filename, "w+b");
-    if (treeFile != NULL) {
-        // insert(root, 7);
-        for (i = INS_MAX - 1; i >= 0; i--) {
-            insert(&root, i, NIL);
-        }
-    }
-    else {
-        printf("Bullshit\n");
-    }
-
-    // printTree(root);
-    graphTree(root);
-
-    free(root);
-    root = NULL;
-    return 0;
 }
 
 void toString(node_t *node) {
@@ -337,11 +161,222 @@ void printTree(node_t *node) {
             child = (node_t *) malloc(sizeof (node_t));
             read(child, node -> key[i][PTR]);
 
-            printf("%d\n", i);
-
             printTree(child);
             free(child);
             child = NULL;
         }
     }
+}
+
+
+// ?
+// long floorEntry(node_t *node, long key) {
+//     int i;
+//
+//     for (i = 0; i < B; i++) {
+//         if (node -> key[i][INFO] == key) {
+//             return i;
+//         }
+//         else if (node -> key[i][INFO] > key) {
+//             return i - 1;
+//         }
+//     }
+//
+//     return NIL;
+// }
+
+node_t * find(node_t *node, long key) {
+    int i;
+    long index;
+
+    if (node != NULL) {
+        for (i = 0; i < B; i++) {
+            if (node -> key[i][INFO] == key) {
+                if (node -> key[i][PTR] != NIL) {
+                    index = node -> key[i][PTR];
+                    node = (node_t *) malloc(sizeof (node_t));
+                    read(node, index);
+                    i = -1;
+                }
+                else {
+                    return node;
+                }
+            }
+            else if (node -> key[i][INFO] > key) {
+                if (i > 0 && node -> key[i - 1][PTR] != NIL) {
+                    index = node -> key[i - 1][PTR];
+                    node = (node_t *) malloc(sizeof (node_t));
+                    read(node, index);
+                    i = -1;
+                }
+                else {
+                    if (node -> key[i][PTR] != NIL) {
+                        index = node -> key[i][PTR];
+                        node = (node_t *) malloc(sizeof (node_t));
+                        read(node, index);
+                        i = -1;
+                    }
+                    else {
+                        return node;
+                    }
+                }
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void insert(node_t **node, long key, long ptr) {
+    int i;
+
+    if (*node == NULL) *node = newNode();
+
+    for (i = 0; i < B; i++) {
+        if ((*node) -> key[i][INFO] == (long) INFTY) {
+            (*node) -> key[i][INFO] = key;
+            (*node) -> key[i][PTR] = ptr;
+            (*node) -> count++;
+            // the matrix to sort, quantity of lines, size of each line, comparing function
+            qsort((*node) -> key, B, sizeof (long) * 2, &cmp);
+            break;
+        }
+    }
+
+    addFix(node);
+    write(*node, (*node) -> index);
+}
+
+void addFix(node_t **node) {
+    int i, j;
+    long firstKey = NIL;
+    node_t *parent = NULL, *left = NULL, *right = NULL, *tmp = NULL;
+
+    if ((*node) -> parent != NIL) {
+        parent = (node_t *) malloc(sizeof (node_t));
+        read(parent, (*node) -> parent);
+
+        if (parent -> key[0][INFO] > (*node) -> key[0][INFO]) {
+            parent -> key[0][INFO] = (*node) -> key[0][INFO];
+        }
+
+        write(parent, parent -> index);
+        free(parent); parent = NULL;
+    }
+
+    if ((*node) -> count >= B) {
+        firstKey = (*node) -> key[0][INFO];
+
+        for (i = 0; i < B / 2; i++) {
+            insert(&left, (*node) -> key[i][INFO], (*node) -> key[i][PTR]);
+
+            // updating children's parent
+            if (left -> key[i][PTR] != NIL) {
+                tmp = (node_t *) malloc(sizeof (node_t));
+                read(tmp, left -> key[i][PTR]);
+
+                tmp -> parent = left -> index;
+                write(tmp, left -> key[i][PTR]);
+
+                free(tmp); tmp = NULL;
+            }
+        }
+
+        for (j = 0; i < B; i++, j++) {
+            insert(&right, (*node) -> key[i][INFO], (*node) -> key[i][PTR]);
+
+            // updating children's parent
+            if (right -> key[i][PTR] != NIL) {
+                tmp = (node_t *) malloc(sizeof (node_t));
+                read(tmp, right -> key[i][PTR]);
+
+                tmp -> parent = right -> index;
+                write(tmp, right -> key[i][PTR]);
+
+                free(tmp); tmp = NULL;
+            }
+        }
+
+        // it's the root of the tree
+        if ((*node) -> parent == NIL) {
+            free(*node); *node = NULL;
+
+            // first keys of left and right
+            insert(node, left -> key[0][INFO], left -> index);
+            insert(node, right -> key[0][INFO], right -> index);
+
+            left -> parent = (*node) -> index;
+            right -> parent = (*node) -> index;
+
+            write((*node), (*node) -> index);
+        }
+        else {
+            parent = (node_t *) malloc(sizeof (node_t));
+            read(parent, (*node) -> parent);
+
+            delete(&parent, firstKey);
+            insert(&parent, left -> key[0][INFO], left -> index);
+            insert(&parent, right -> key[0][INFO], right -> index);
+
+            left -> parent = parent -> index;
+            right -> parent = parent -> index;
+
+            write(parent, parent -> index);
+            free(parent); parent = NULL;
+        }
+
+        write(left, left -> index);
+        free(left); left = NULL;
+
+        write(right, right -> index);
+        free(right); right = NULL;
+    }
+
+    if ((*node) -> parent != NIL) {
+        parent = (node_t *) malloc(sizeof (node_t));
+        read(parent, (*node) -> parent);
+
+        addFix(&parent);
+
+        write(parent, parent -> index);
+        free(parent); parent = NULL;
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int i;
+    char filename[] = "tree.bin";
+    node_t *root = NULL, *node = NULL;
+
+    diskIndex = 0;
+    rootIndex = NIL;
+    // root = newNode();
+
+    treeFile = fopen(filename, "w+b");
+    if (treeFile != NULL) {
+        for (i = INS_MAX - 1; i >= 0; i--) {
+        // for (i = 0; i < INS_MAX; i++) {
+            if (rootIndex >= 0) {
+                root = (node_t *) malloc(sizeof (node_t));
+                read(root, rootIndex);
+            }
+            node = find(root, i);
+            insert(&node, i, NIL);
+
+            root = (node_t *) malloc(sizeof (node_t));
+            read(root, rootIndex);
+            graphTree(root);
+        }
+    }
+    else {
+        printf("Bullshit\n");
+    }
+
+    read(root, rootIndex);
+    // printTree(root);
+    graphTree(root);
+
+    free(root);
+    root = NULL;
+    return 0;
 }
